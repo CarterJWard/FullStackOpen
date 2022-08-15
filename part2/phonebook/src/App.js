80f6ffda6517eb9f0df3117d0d9be62fedabda76
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2022-08-15 19:07:36 +1000

    Phone book step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const People = ({ people }) => {
   return (
@@ -83,14 +84,19 @@ const Search = ({ persons, setPeopleDisplay }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
-  const [peopleDisplay, setPeopleDisplay] = useState(persons)
+  const [peopleDisplay, setPeopleDisplay] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        response = response.data
+        setPersons(response)
+        setPeopleDisplay(response)
+      })
+  }, []);
 
   return (
     <div>
